ed95d69942bf62ea1d3d2ec9019b1aa6b06b8d76
eppleton <info@eppleton.de>
2015-04-29 08:03:56 +0200

    comments

--- client/src/main/java/com/dukescript/enzods/Main.java
+++ client/src/main/java/com/dukescript/enzods/Main.java
@@ -5,15 +5,16 @@ import net.java.html.boot.BrowserBuilder;
 import net.java.html.json.Models;
 
 public final class Main {
+
     private Main() {
     }
-    
+
     public static void main(String... args) throws Exception {
         BrowserBuilder.newBrowser().
-            loadPage("pages/index.html").
-            loadClass(Main.class).
-            invoke("onPageLoad", args).
-            showAndWait();
+                loadPage("pages/index.html").
+                loadClass(Main.class).
+                invoke("onPageLoad", args).
+                showAndWait();
         System.exit(0);
     }
 
@@ -21,10 +22,12 @@ public final class Main {
      * Called when the page is ready.
      */
     public static void onPageLoad() throws Exception {
-       LEDViewModel ledViewModel = new LEDViewModel(true, "container");
-       Models.toRaw(ledViewModel);
+        LEDViewModel ledViewModel = new LEDViewModel(true, "container");
+       // this call to Models.toRaw initializes Knockout
+       // so we can register the LED component.
+        Models.toRaw(ledViewModel);
         LED.registerLED();
         ledViewModel.applyBindings();
     }
-    
+
 }
