43926284f6921e4888b1bc458223f3e463e1aebc
eppleton <info@eppleton.de>
2015-04-29 07:46:27 +0200

    removed superfluous code (was used to directly create a control)

--- js/src/main/java/com/dukescript/enzods/led/LED.java
+++ js/src/main/java/com/dukescript/enzods/led/LED.java
@@ -11,31 +11,6 @@ import net.java.html.js.JavaScriptResource;
 @JavaScriptResource(value = "led-min.js")
 public class LED {
 
-    Object wrapped;
-
-    public LED(String ledID, String targetID, int w, int h, boolean scalable, boolean frameVisible, boolean on, String col) {
-        this.wrapped = createLed(ledID, targetID, w, h, scalable, frameVisible, on, col);
-    }
-
-    public void setOn(boolean on) {
-        setOnImpl(wrapped, on);
-    }
-
-    @JavaScriptBody(args = {"wrapped", "on"}, body = "wrapped.setOn( on );")
-    public static native void setOnImpl(Object wrapped, Boolean on);
-
-    @JavaScriptBody(args = {"ledID", "targetID", "w", "h", "scalable", "frameVisible", "on", "col"}, body = ""
-            + "return new Led({id: ledID,\n"
-            + "                                 parentId      : targetID,\n"
-            + "                                 width         : w,\n"
-            + "                                 height        : h,\n"
-            + "                                 isScalable    : scalable,\n"
-            + "                                 isFrameVisible: frameVisible,\n"
-            + "                                 isOn          : on,\n"
-            + "                                 color         : col\n"
-            + "                               });")
-    private static native Object createLed(String ledID, String targetID, int w, int h, boolean scalable, boolean frameVisible, boolean on, String col);
-
     @JavaScriptBody(args = {}, body = "ko.components.register('led', {\n"
             + "    viewModel: function(params){\n"
             + "        this.on = params.on;\n"
